2779a109a72cbf7800f7b3e84ce65b5bbe7df15b
Yiddy <yidkon@gmail.com>
2025-08-14 15:27:38 -0400

    fix: add prop type validations to resolve #30

--- src/scenes/Home/Block/index.jsx
+++ src/scenes/Home/Block/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import cl from 'classnames';
+import PropTypes from 'prop-types';
 import s from './index.module.css';
 import NumberDisplay from '../NumberDisplay';
 
@@ -7,11 +8,21 @@ function Block({ className, value, title }) {
   return (
     <div className={cl(s.root, className)}>
       <div className={s.numbers}>
-        {value.split('').map((v, k) => <NumberDisplay value={+v} key={k} />)}
+        {value.split('').map((v) => <NumberDisplay value={+v} key={v} />)}
       </div>
       <div className={s.title}>{title}</div>
     </div>
   );
 }
+
+Block.propTypes = {
+  className: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+Block.defaultProps = {
+  className: '',
+};
 
 export default Block;
