1f35818f44a1e2a1ec00cdcee2f96410ca5377f3
Yiddy <yidkon@gmail.com>
2025-09-02 18:03:32 -0400

    Fixed sizing issues caused by tailwind border-box
    As a result the padding was included in the Block element sizing instead of adding it.
    
    Fixed it by raising the size to include the padding size. The sizing was also changed to use tailwind utility class sizes instead of custom values.

--- src/scenes/Home/Block/index.tsx
+++ src/scenes/Home/Block/index.tsx
@@ -12,11 +12,11 @@ function Block({ className = '', value, title }: BlockProps) {
   return (
     <div
       className={cl(
-        'flex h-[70px] w-[140px] flex-col items-center justify-center rounded-md bg-[rgba(211,211,211,0.349)] p-12 shadow-[0px_5px_33px_-13px_rgba(0,0,0,0.5)]',
+        'flex h-44 w-60 flex-col items-center justify-center rounded-md bg-[rgba(211,211,211,0.349)] p-12 shadow-[0px_5px_33px_-13px_rgba(0,0,0,0.5)]',
         className
       )}
     >
-      <div className="flex min-h-[100px] items-center justify-center">
+      <div className="flex min-h-24 items-center justify-center">
         {value.split('').map((v, k) => (
           <NumberDisplay value={+v} key={k} />
         ))}
--- src/scenes/Home/NumberDisplay/index.tsx
+++ src/scenes/Home/NumberDisplay/index.tsx
@@ -16,15 +16,15 @@ function NumberDisplay({ value: v }: NumberDisplayProps) {
   const value = fd ? Math.floor(Math.random() * 11) : v;
 
   return (
-    <div className="relative h-[100px] w-[70px] overflow-hidden text-[100px]">
+    <div className="relative h-24 w-16 overflow-hidden text-8xl">
       <div
-        className="relative h-full w-full transition-all duration-[800ms]"
-        style={{ transform: `translateY(-${value * 100}px)` }}
+        className="relative h-full w-full transition-transform duration-[800ms]"
+        style={{ transform: `translateY(-${value * 96}px)` }}
       >
         {ten.map(t => (
           <div
-            className="absolute right-0 left-0 h-[50px] font-bold text-white"
-            style={{ top: `${t * 100}px` }}
+            className="absolute right-0 left-0 h-12 font-bold text-white"
+            style={{ top: `${t * 96}px` }}
             key={t}
           >
             {t}
