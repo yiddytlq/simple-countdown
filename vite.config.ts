3563a9af2d80600aaf8812ec657091169680f040
Yiddy <yidkon@gmail.com>
2025-09-02 15:06:14 -0400

    Installed tailwindcss
    We installed tailwindcss as a vite plugin. Therefore there is no need for postcss & autoprefixer.
    
    Resolves #44

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 
   // Build configuration to match current Create React App output
   build: {
